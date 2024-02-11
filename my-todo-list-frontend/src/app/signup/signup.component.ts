849ab35404bd520086c1e28acd6714514e03db44
Sajan Sthapit <sajansthapit99@gmail.com>
2024-02-11 21:44:31 +0545

    add: validations in signup form

--- my-todo-list-frontend/src/app/signup/signup.component.ts
+++ my-todo-list-frontend/src/app/signup/signup.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordStrengthComponent } from '../shared/password-strength/password-strength.component';
@@ -8,24 +13,47 @@ import { PasswordStrengthComponent } from '../shared/password-strength/password-
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule, ButtonModule, InputTextModule, ReactiveFormsModule, PasswordStrengthComponent],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    PasswordStrengthComponent,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignupComponent {
-
+export class SignupComponent implements OnInit {
   validPassword: boolean = false;
+  isPasswordMatched: boolean = false;
 
   signupForm = new FormGroup({
-    fullName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    reTypePassword: new FormControl('')
+    fullName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    reTypePassword: new FormControl('', [Validators.required]),
   });
 
-  signup():void {
+  ngOnInit(): void {
+    this.signupForm.get('password')?.valueChanges.subscribe((password) => {
+      this.isPasswordMatched = password == this.signupForm.value.reTypePassword;
+    });
 
+    this.signupForm
+      .get('reTypePassword')
+      ?.valueChanges.subscribe((reTypePassword) => {
+        this.isPasswordMatched =
+          reTypePassword == this.signupForm.value.password;
+      });
+  }
+
+  signup(): void {
+    if (!this.signupForm.valid) this.signupForm.markAllAsTouched();
+
+    if (this.signupForm.valid && this.validPassword && this.isPasswordMatched) {
+      console.log(this.signupForm.value);
+    }
   }
 
   isPasswordValid(event: boolean): void {
